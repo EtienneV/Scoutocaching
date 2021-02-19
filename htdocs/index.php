1f4bb5ee85f54f0df17fc1f6661aceadb93f8a1e
Etienne Villedieu <EtienneV@users.noreply.github.com>
2021-02-19 12:08:02 +0100

    lyonlevant server

---
+++ htdocs/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+<head> 	
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<link rel="stylesheet" type="text/css" href="https://yulpa.io/assets/css/style.css" media="screen"> 
+<link rel="stylesheet" type="text/css" href="https://yulpa.io/assets/plugins/bootstrap/css/bootstrap.min.css">	
+<link rel="stylesheet" type="text/css" href="https://yulpa.io/assets/css/theme-colors/red.css">
+<link rel="stylesheet" type="text/css" href="https://yulpa.io/assets/plugins/font-awesome/css/font-awesome.min.css">
+
+<title>yulPa : Scoutocaching !</title>
+</head> 
+ <body> 	
+
+			<div class="container">
+				<div class="headline-center-v2 headline-center-v2-dark ">												<div class="img-responsive">										<img src="http://geocaching.sgdf-lyonlevant.fr/i287104489302534209._szw1280h1280_.png">									</div>
+					<h2>Le service a été créé avec succès !</h2>
+					<span class="bordered-icon"><i class="fa fa-star"></i></span>
+				</div>
+				<div class="row">
+						<div class="col-md-3">
+							<div class="img-responsive">
+								<img src="https://www.yulpa.io/images/home/logo-yulpa.png">
+							</div>
+						</div>
+						<div class="col-md-6">
+						<div class="margin-bottom-30">
+							<div class="info-description">
+ 							<p>Le nom de domaine <span class="url">geocaching.sgdf-lyonlevant.fr</span> a correctement été ajouté sur votre hébergement.</p> 	
+ 							 <p>Rendez-vous sur le <a href="https://my.yulpa.io/">Manager iWAL</a> pour créer un compte FTP et retirer cette page (<span class="url">index.php</span>).</p> 			
+ 							 <p>Après connexion sur le <a href="https://my.yulpa.io/">Manager</a>, vous pourrez gérer vos comptes e-mails, bases de données, ... et bien plus encore. <br /><br /> 
+ 							<p><b>Toute l'équipe de yul<span style="color:#d40a0a;">P</span>a vous remercie pour votre confiance</b></p> 
+ 														<p><a href="https://yulpa.io">Site yulPa</a>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://forums.yulpa.io/">Forums</a>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://docs.yulpa.io/">Documentations</a>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://my.yulpa.io/">Manager iWAL</a>&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://travaux.yulpa.io/">Travaux</a></p> 
+							</div>
+						</div>
+		
+					</div>
+
+				</div><!--/row-->
+
+			</div><!--/container-->
+
+		
+</body> 
+</html>
